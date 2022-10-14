21848f6f9256686395af4aeccfcf12f6438f4cd7
7ngenious <zxvs1218@gmail.com>
2022-10-14 15:34:43 +0900

    develop: react project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
-
+import React from 'react';
+import { useState } from 'react';
 const Home = () => {
-  return <div className="Wrap">안녕하세요!</div>;
+  const [count, setCount] = useState(0);
+
+  const onClick = () => {
+    setCount(count + 1);
+    setCount(count + 1);
+    console.dir(count);
+  };
+
+  // const onClick = () => {
+  //   setCount((count) => count + 1);
+  //   setCount((count) => count + 1);
+  //   console.dir(count);
+  // };
+  return (
+    <div className='Wrap'>
+      <div>{count}</div>
+      <button onClick={onClick}>+1</button>
+    </div>
+  );
 };
 
 export default Home;
